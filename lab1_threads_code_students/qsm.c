6c20137204f7d4e454b5f5c6630149d758eff04d
Miran Ismail <90042468+MiranIsmail@users.noreply.github.com>
2023-11-28 09:00:41 +0100

    Update qsm.c
    final

--- lab1_threads_code_students/qsm.c
+++ lab1_threads_code_students/qsm.c
@@ -1,6 +1,7 @@
 /***************************************************************************
  *
- * "multithreaded qs with threadpool time mesuared inside 1,8s without init_array()"
+ * "multithreaded quicksort with threadpool time mesuared outside with "time" 2,5s"
+ * if we omit the init_array() the time is roughly 1,7s
  *
  ***************************************************************************/
 
@@ -22,16 +23,16 @@
 
 static int *v;
 /*****************************************************************/
-#define NUM_TASKS 16
-#define MAX_THREADS 32
-#define GRANULARITY 200000
+#define NUM_TASKS 16       // how many tasks can be in the queue this affects the granularity somewhat but not in a big way
+#define MAX_THREADS 32     // how many threads can be running at the same time
+#define GRANULARITY 200000 // this is the minimum size of a task if a task is smaller than this it will be executed by the thread that took it
 typedef struct Task
 {
     unsigned low;
     unsigned high;
     int *v;
 
-} Task;
+} Task; // this is the struct that holds the task args
 Task task_q[NUM_TASKS];
 int task_count = 0;
 pthread_mutex_t taking_q_mutex;
@@ -45,7 +46,7 @@ void add_task(Task task)
     task_q[task_count] = task;
     task_count++;
     pthread_cond_signal(&no_task_cond);
-}
+} // this function adds a task to the queue and signals the threads that there is a new task in the queue
 /*****************************************************************/
 
 static void
@@ -145,7 +146,7 @@ void exe_task(Task *task)
             exe_task(&task_right);
         }
     }
-}
+} // this function executes a task and if the task is bigger than the granularity it will add the subtasks to the queue otherwise it will execute the subtasks itself
 int check_if_sorted(int *v, int size)
 {
     for (int i = 0; i < size - 1; i++)
@@ -156,7 +157,7 @@ int check_if_sorted(int *v, int size)
         }
     }
     return 1; // Return 1 if the array is sorted
-}
+} // this function checks if the array is sorted if it is it returns 1 otherwise it returns 0
 void *start_thread()
 {
     Task task;
@@ -180,7 +181,7 @@ void *start_thread()
             pthread_mutex_unlock(&taking_q_mutex);
             pthread_mutex_lock(&no_task_mutex);
             num_of_threads_running--;
-            if (num_of_threads_running == 0)
+            if (num_of_threads_running == 0) // if there is no task in the queue and no threads running it will exit. this uses a mutex and a condition variable
             {
                 pthread_mutex_unlock(&no_task_mutex);
                 pthread_cond_broadcast(&no_task_cond);
@@ -198,15 +199,16 @@ void *start_thread()
     }
 
     return NULL;
-}
+} // this function is the function that the threads run it takes a task from the queue and executes it if there is no task in the queue it will wait for a signal from the main thread
+  // and if there is no task in the queue and no threads running it will exit
 
 int main(int argc, char **argv)
 {
-    struct timeval start, end;
-    long mtime, seconds, useconds;
+    // struct timeval start, end;
+    // long mtime, seconds, useconds;
 
     init_array();
-    gettimeofday(&start, NULL);
+    // gettimeofday(&start, NULL);
     pthread_mutex_init(&taking_q_mutex, NULL);
     pthread_mutex_init(&no_task_mutex, NULL);
     pthread_cond_init(&no_task_cond, NULL);
@@ -216,7 +218,7 @@ int main(int argc, char **argv)
     task.high = MAX_ITEMS - 1;
     task.low = 0;
     task.v = v;
-    add_task(task);
+    add_task(task); // add the array to the queue as a task
     int i;
 
     for (i = 0; i < MAX_THREADS; i++)
@@ -234,13 +236,13 @@ int main(int argc, char **argv)
     pthread_mutex_destroy(&no_task_mutex);
     pthread_cond_destroy(&no_task_cond);
 
-    gettimeofday(&end, NULL);
-    seconds = end.tv_sec - start.tv_sec;
-    useconds = end.tv_usec - start.tv_usec;
-    mtime = ((seconds) * 1000 + useconds / 1000.0) + 0.5;
-    printf("Elapsed time: %ld milliseconds\n", mtime);
+    // gettimeofday(&end, NULL);
+    // seconds = end.tv_sec - start.tv_sec;
+    // useconds = end.tv_usec - start.tv_usec;
+    // mtime = ((seconds) * 1000 + useconds / 1000.0) + 0.5;
     // if (check_if_sorted(v, MAX_ITEMS))
     // {
-    //     printf("sorted");
+    //     printf("sorted\n");
     // }
+    // printf("Elapsed time: %ld milliseconds\n", mtime);
 }
