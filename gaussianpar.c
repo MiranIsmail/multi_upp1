edcba12dca2c88e414907001bc3640303406f1d9
WavenXD <artursbiten@gmail.com>
2024-11-21 10:41:06 +0100

    better

--- gaussianpar.c
+++ gaussianpar.c
@@ -1,83 +1,107 @@
 /***************************************************************************
  *
  * Parallel version of Gaussian elimination
- * Don't recreate threads, no deadlock
+ *
  ***************************************************************************/
 #include <stdio.h>
 #include <stdlib.h>
-#include "pthread.h"
-#include <math.h>
-#include <string.h>
-
-#define MAX_SIZE 4096
-#define NUM_CORES 8
+#include <pthread.h>
+#define MAX_SIZE 2048
+#define NUM_CORES 16
 
 typedef double matrix[MAX_SIZE][MAX_SIZE];
-
-typedef struct {
-    int thread_id;
-    int start_row;
-    int end_row;
-    int k;
-    int active;
-} threadArgs;
-
-int N;              /*matrix size */
-int maxnum;         /*max number of element*/
-char *Init;         /*matrix init type */
-int PRINT;          /*print switch */
-matrix A;           /*matrix A */
-double b[MAX_SIZE]; /*vector b */
-double y[MAX_SIZE]; /*vector y */
-
-pthread_mutex_t mutexes_1[NUM_CORES];
-pthread_mutex_t mutexes_2[NUM_CORES];
-
-
-/*forward declarations */
+int N;              /* matrix size */
+int maxnum;         /* max number of element*/
+char *Init;         /* matrix init type */
+int PRINT;          /* print switch */
+matrix A;           /* matrix A */
+double b[MAX_SIZE]; /* vector b */
+double y[MAX_SIZE]; /* vector y */
+/* forward declarations */
 void work(void);
-void *gaussian_row(void *params);
 void Init_Matrix(void);
 void Print_Matrix(void);
 void Init_Default(void);
 int Read_Options(int, char **);
 
+
+pthread_mutex_t *mutex_start; //Signals thread to start
+pthread_mutex_t *mutex_done; //Signals that thread finished
+
+typedef struct {
+    int thread_id;
+    int start_row;
+    int end_row;
+    int active;
+    int pivot;
+} threadArgs;
+
+void init_mutexes() {
+    mutex_start = malloc(NUM_CORES * sizeof(pthread_mutex_t));
+    mutex_done = malloc(NUM_CORES * sizeof(pthread_mutex_t));
+    for (int i = 0; i < NUM_CORES; i++) {
+        pthread_mutex_init(&mutex_start[i], NULL);
+        pthread_mutex_init(&mutex_done[i], NULL);
+    }
+}
+
+void *gaussian_row(void *params)
+{
+    threadArgs *args = (threadArgs *)params;
+    int curr = args->thread_id;
+
+    pthread_mutex_unlock(&mutex_done[curr]);
+    while (args->active == 1) {// Wait for signal
+        pthread_mutex_lock(&mutex_start[curr]);
+        pthread_mutex_unlock(&mutex_start[curr]);
+        if (args->active == 0){ //no longer active
+            return NULL;
+        }
+
+        int start_row = args->start_row;
+        int end_row = args->end_row;
+        int k = args->pivot;
+        for (int i = start_row; i < end_row && i < N; i++) {
+            for (int j = k + 1; j < N; j++)
+                A[i][j] = A[i][j] - A[i][k] * A[k][j]; /*Elimination step */
+            b[i] = b[i] - A[i][k] * y[k];
+            A[i][k] = 0.0;
+        }
+        // Thread is done
+        pthread_mutex_lock(&mutex_start[curr]);
+        pthread_mutex_unlock(&mutex_done[curr]);
+    }
+}
+
 int main(int argc, char **argv)
 {
-    for (int j = 0; j < NUM_CORES; j++) {
-        pthread_mutex_init(&mutexes_1[j], NULL);
-        pthread_mutex_init(&mutexes_2[j], NULL);
-    }
-    Init_Default();           /*Init default values */
-    Read_Options(argc, argv); /*Read arguments */
-    Init_Matrix();            /*Init the matrix */
+    init_mutexes(); //Initialize mutexes, HELPER FUNCTION
+
+    Init_Default();           /* Init default values */
+    Read_Options(argc, argv); /* Read arguments */
+    Init_Matrix();            /* Init the matrix */
     work();
     if (PRINT == 1)
         Print_Matrix();
 }
 
-void work(void)
-{
-    pthread_t threads[NUM_CORES];
+void work(void){
     threadArgs args[NUM_CORES];
+    pthread_t children[NUM_CORES];
 
     int rows_per_thread = (N + NUM_CORES - 1) / NUM_CORES;
-    for (int t = 0; t < NUM_CORES; t++) { // start_row threads
+    for (int t = 0; t < NUM_CORES; t++){
         args[t].thread_id = t;
         args[t].start_row = t * rows_per_thread;
-        args[t].end_row = ((t + 1) * rows_per_thread - 1 < N - 1) ?
-                  (t + 1) * rows_per_thread - 1 :
-                  N - 1;
-        args[t].k = -1;
+        int end = (t + 1) * rows_per_thread - 1; //block number * rows
+        if (end < N - 1) { //if end does not go out of range
+            args[t].end_row = end;
+        } else {
+            args[t].end_row = N - 1; //last row
+        }
         args[t].active = 1;
-        pthread_mutex_lock(&mutexes_2[t]);
-        pthread_mutex_lock(&mutexes_1[t]);
-        pthread_create(&threads[t], NULL, gaussian_row, &args[t]);
-    }
-    // Wait until all threads has start_rowed
-    for (int i = 0; i < NUM_CORES; i++) {
-        pthread_mutex_lock(&mutexes_2[i]);
-        pthread_mutex_unlock(&mutexes_2[i]);
+
+        pthread_create(&(children[t]), NULL, gaussian_row, (void *)&args[t]);
     }
     for (int k = 0; k < N; k++){ /*Outer loop */
         double diag = A[k][k]; //diagonal
@@ -87,54 +111,34 @@ void work(void)
         A[k][k] = 1.0;
         int div = N / NUM_CORES; //division
         int counter = 0;
-        for (int i = k + 1; i < N; i += div)
-        {
+        for (int i = k + 1; i < N; i += div){
+            args[counter].pivot = k;
             args[counter].start_row = i;
             args[counter].end_row = i + div;
-            args[counter].k = k;
-            pthread_mutex_lock(&mutexes_2[counter]);
-            pthread_mutex_unlock(&mutexes_1[counter]); // start_row thread execution
+            pthread_mutex_lock(&mutex_done[counter]);
+            pthread_mutex_unlock(&mutex_start[counter]);
             counter++;
         }
         for (int j = counter; j < NUM_CORES; j++) {
-            if (args[j].active == 1) {
+            if (args[j].active == 1) { //Terminate thread
                 args[j].active = 0;
-                pthread_mutex_unlock(&mutexes_1[j]); // Allow unused threads to exit
-                pthread_join(threads[j], NULL);
+                pthread_mutex_unlock(&mutex_start[j]); //Threads are waiting
+                pthread_join(children[j], NULL);
             }
         }
         // Wait for threads to finish
-        for (int j = 0; j < counter; j++) {
-            pthread_mutex_lock(&mutexes_2[j]);
-            pthread_mutex_unlock(&mutexes_2[j]);
+        for (int h = 0; h < counter; h++) {
+            pthread_mutex_lock(&mutex_done[h]);
+            pthread_mutex_unlock(&mutex_done[h]);
         }
     }
-}
-
-void *gaussian_row(void *params){
-    threadArgs *args = (threadArgs *)params;
-    int n = args->thread_id;
-    pthread_mutex_unlock(&mutexes_2[n]);
-    while (args->active == 1) {
-        // Wait for program to signal run
-        pthread_mutex_lock(&mutexes_1[n]);
-        pthread_mutex_unlock(&mutexes_1[n]);
-        if (args->active == 0) return NULL;
-        int start_row = args->start_row;
-        int end_row = args->end_row;
-        int k = args->k;
-        for (int i = start_row; i < end_row && i < N; i++) {
-            for (int j = k + 1; j < N; j++)
-                A[i][j] = A[i][j] - A[i][k] * A[k][j]; /*Elimination step */
-            b[i] = b[i] - A[i][k] * y[k];
-            A[i][k] = 0.0;
-        }
-        // Signal thread finished
-        pthread_mutex_lock(&mutexes_1[n]);
-        pthread_mutex_unlock(&mutexes_2[n]);
+    for (int i = 0; i < NUM_CORES; i++) {
+        pthread_mutex_destroy(&mutex_start[i]);
+        pthread_mutex_destroy(&mutex_done[i]);
     }
+    free(mutex_start);
+    free(mutex_done);
 }
-
 void Init_Matrix()
 {
     int i, j;
@@ -148,7 +152,7 @@ void Init_Matrix()
         {
             for (j = 0; j < N; j++)
             {
-                if (i == j) /*diagonal dominance */
+                if (i == j) /* diagonal dominance */
                     A[i][j] = (double)(rand() % maxnum) + 5.0;
                 else
                     A[i][j] = (double)(rand() % maxnum) + 1.0;
@@ -161,14 +165,14 @@ void Init_Matrix()
         {
             for (j = 0; j < N; j++)
             {
-                if (i == j) /*diagonal dominance */
+                if (i == j) /* diagonal dominance */
                     A[i][j] = 5.0;
                 else
                     A[i][j] = 2.0;
             }
         }
     }
-    /*Initialize vectors b and y */
+    /* Initialize vectors b and y */
     for (i = 0; i < N; i++)
     {
         b[i] = 2.0;
@@ -178,7 +182,6 @@ void Init_Matrix()
     if (PRINT == 1)
         Print_Matrix();
 }
-
 void Print_Matrix()
 {
     int i, j;
@@ -200,7 +203,6 @@ void Print_Matrix()
     printf("]\n");
     printf("\n\n");
 }
-
 void Init_Default()
 {
     N = 2048;
@@ -208,7 +210,6 @@ void Init_Default()
     maxnum = 15.0;
     PRINT = 0;
 }
-
 int Read_Options(int argc, char **argv)
 {
     char *prog;
@@ -237,7 +238,7 @@ int Read_Options(int argc, char **argv)
             case 'D':
                 printf("\nDefault: n = %d ", N);
                 printf("\n Init = rand");
-                printf("\n maxnum = 15 ");
+                printf("\n maxnum = 5 ");
                 printf("\n P = 0 \n\n");
                 exit(0);
                 break;
